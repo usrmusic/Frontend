be386028bb037a6c91aad8174647a825f85cd098
idevjunaid <idevjunaid@gmail.com>
2026-02-10 00:13:55 +0500

    Update Sidebar and layout components to link logout and include session in Header

--- src/app/(authenticated)/_components/Sidebar.tsx
+++ src/app/(authenticated)/_components/Sidebar.tsx
@@ -73,7 +73,7 @@ const Sidebar = () => {
       icon: <Settings />,
     },
     {
-      href: "#",
+      href: "/auth/logout",
       icon: <Logout />,
     },
   ];
--- src/app/(authenticated)/layout.tsx
+++ src/app/(authenticated)/layout.tsx
@@ -1,14 +1,17 @@
 import { PropsWithChildren } from "react";
 import Sidebar from "./_components/Sidebar";
 import Header from "./_components/Header";
+import { auth0 } from "@/src/lib/auth0";
 
-const layout = ({ children }: PropsWithChildren) => {
+const layout = async ({ children }: PropsWithChildren) => {
+  const session = await auth0.getSession();
+
   return (
     <div className="p-6">
       <div className="bg-secondary-200 rounded-3xl min-h-[calc(100vh-48px)] p-8 overflow-y-auto max-h-[calc(100vh-48px)]">
         <Sidebar />
         <div className="ml-25">
-          <Header />
+          <Header session={session} />
           <main>{children}</main>
         </div>
       </div>
