0fda47103c0bdab2148571a88fab27a60372c9de
idevjunaid <idevjunaid@gmail.com>
2026-03-29 20:59:18 +0500

    fix: wrap UsersHeader in Suspense for improved loading handling

--- src/app/(authenticated)/(users)/layout.tsx
+++ src/app/(authenticated)/(users)/layout.tsx
@@ -1,11 +1,13 @@
-import { ReactNode } from "react";
+import { ReactNode, Suspense } from "react";
 import UsersHeader from "./_components/UsersHeader";
 
 const layout = ({ children }: { children: ReactNode }) => {
   return (
     <>
       <div className="space-y-4 mt-4">
-        <UsersHeader />
+        <Suspense fallback={<div />}>
+          <UsersHeader />
+        </Suspense>
         {children}
       </div>
     </>
