75a019c8820de993e618760ace24efb6fdcd0c3b
idevjunaid <idevjunaid@gmail.com>
2026-04-05 20:43:14 +0500

    Refactor report and file upload columns for consistency in data indexing

--- src/app/(authenticated)/admin-report/page.tsx
+++ src/app/(authenticated)/admin-report/page.tsx
@@ -64,7 +64,6 @@ const Page = () => {
     },
   ];
 
-  console.log(reportData, "reportData");
   return (
     <div className="mt-4 space-y-4">
       <div className="flex items-center justify-between">
